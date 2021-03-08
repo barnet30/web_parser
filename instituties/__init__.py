a47ca5f7c7b2b5ce3fc6930ef4d8867a01def0da
Павел Гавриличев <PNGavrilichev@stud.kpfu.ru>
2021-03-08 16:59:54 +0300

    add philolog

---
+++ instituties/__init__.py
@@ -0,0 +1,7 @@
+# from .law import parse_law
+# from .psychology import parse_psychology
+# from .ivmiit import parse_ivmiit
+# from .mehmat import parse_mehmat
+# from .chemistry import parse_chemistry
+# from .ecology import parse_ecology
+# from .physics import parse_physics
